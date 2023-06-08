7bcde5ccbad63fd3ac9d9d946dead4a3b79df834
mohankaushik97 <41466343+mohankaushik97@users.noreply.github.com>
2023-06-08 14:48:41 -0700

    s

---
+++ 1Participants/P1MohanRun1/matlab/Template/trimAcc.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+clc;
+
+% Specify the path to the Excel file and sheet name
+filename = 'emgAndAcc.xlsx';
+sheet = 'Sheet1';
+
+% Read the data from the Excel sheet into a table
+data = readtable(filename, 'Sheet', sheet);
+
+% Extract the desired columns as column matrices for 'emg.mat'
+Xs = data.Xs;
+emg = data.emg;
+emg_rms = data.emg_rms;
+save('emg.mat', 'Xs', 'emg', 'emg_rms');
+
+% Extract the desired columns as column matrices for 'acc.mat'
+Xs_acc = data.Xs_acc;
+AccX = data.AccX;
+AccY = data.AccY;
+AccZ = data.AccZ;
+save('acc.mat', 'Xs_acc', 'AccX', 'AccY', 'AccZ');
+
+
+nonzero_rows = any([AccX, AccY, AccZ], 2);
+Xs_acc = Xs_acc(nonzero_rows);
+AccX = AccX(nonzero_rows);
+AccY = AccY(nonzero_rows);
+AccZ = AccZ(nonzero_rows);
+
+% Save the updated column matrices to 'acc.mat'
+save('acc.mat', 'Xs_acc', 'AccX', 'AccY', 'AccZ');
+
